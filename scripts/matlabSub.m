1b4e65899a1d085ba6e4c7c93f40b8aec31c9698
Nicolas <nigilro@unal.edu.co>
2022-05-13 17:18:47 -0500

    Updated scripts

--- scripts/matlabSub.m
+++ scripts/matlabSub.m
@@ -1,8 +1,8 @@
 clear; clc;
 rosinit;
 
-jointSub=rossubscriber('/joint_states','sensor_msgs/JointState'); %Creacion del suscriptor
-jointSub.LatestMessage
+jointSub=rossubscriber('/dynamixel_workbench/joint_states','sensor_msgs/JointState'); %Creacion del suscriptor
+jointSub.LatestMessage.Position
 
-
+%%
 rosshutdown;
